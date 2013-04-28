7c991f7d955b9864db4d55bb06784e4a98abd2a6
Omar Khulusi <okhulusi@usc.edu>
2013-04-27 18:34:11 -0700

    Slowed Clam down

--- clamPowerUp.cpp
+++ clamPowerUp.cpp
@@ -13,16 +13,16 @@ ClamPowerUp:: ~ClamPowerUp(){
 
 void ClamPowerUp:: act(){
 	counter_++;
-	if(counter_ > 400){
-		vx_ = -1;
-	} else if(counter_ > 200){
-		vx_ = 1;
-	}
+	if(counter_ % 40 == 0){
+		 if(counter_ > 200*40){
+			vx_ = 1;
+		}
 	
-	if(counter_%100 == 0){
-		vy_*=-1;
+		if(counter_%100*40 == 0){
+			vy_*=-1;
+		}
+		setPos(x() + vx_*gameSpeed_, y() + vy_*gameSpeed_);
 	}
-	setPos(x() + vx_*gameSpeed_, y() + vy_*gameSpeed_);
 }
 
 void ClamPowerUp::setSpeed(int gameSpeed){
